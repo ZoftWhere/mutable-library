03816601d5ce63362ab440bca2ecb82041822cc6
Osmund <21172332+osmundf@users.noreply.github.com>
2019-09-19 21:16:47 +0200

    Added overlay list with test.

--- main-java/app/zoftwhere/mutable/data/OverlayList.java
+++ main-java/app/zoftwhere/mutable/data/OverlayList.java
@@ -1,60 +1,82 @@
 package app.zoftwhere.mutable.data;
 
-import app.zoftwhere.mutable.function.ProxyCollection;
-
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.Spliterator;
+import java.util.function.IntFunction;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+import java.util.stream.Stream;
 
-public class OverlayList<E> implements List<E>, ProxyCollection<E> {
-
-    private final List<E> front;
+public class OverlayList<E> implements List<E> {
 
     private final List<E> back;
 
-    private final List<E> proxy;
+    private final List<E> front;
 
     public OverlayList(List<E> front, List<E> back) {
-        this.front = front;
         this.back = back;
-        this.proxy = new ArrayList<>(front.size());
-        this.proxy.addAll(front);
-        for (int i = 0; i <= proxy.size(); i++) {
-            if (proxy.get(i) == null) {
-                proxy.set(i, back.get(i));
+        this.front = new ArrayList<>(front.size());
+        this.front.addAll(front);
+        for (int i = 0; i < this.front.size(); i++) {
+            if (this.front.get(i) == null) {
+                this.front.set(i, back.get(i));
             }
         }
     }
 
     @Override
     public int size() {
-        return proxy.size();
+        return front.size();
     }
 
     @Override
     public boolean isEmpty() {
-        return proxy.isEmpty();
+        return front.isEmpty();
+    }
+
+    @Override
+    public E get(int index) {
+        return front.get(index) != null ? front.get(index) : back.get(index);
+    }
+
+    @Override
+    public E set(int index, E element) {
+        if (element == null) {
+            if (front.get(index) != null) {
+                front.set(index, null);
+                return front.set(index, back.get(index));
+            }
+            return front.get(index);
+        }
+        else {
+            front.set(index, element);
+            return front.set(index, element);
+        }
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return front.indexOf(o);
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return front.lastIndexOf(o);
     }
 
     @Override
     public boolean contains(Object o) {
-        return proxy.contains(o);
+        return front.contains(o);
     }
 
     @Override
-    public Iterator<E> iterator() {
-        return new ProxyIterator<>(this);
-    }
-
-    @Override
-    public Object[] toArray() {
-        return proxy.toArray();
-    }
-
-    public <T> T[] toArray(T[] a) {
-        return proxy.toArray(a);
+    public boolean containsAll(Collection<?> c) {
+        return front.containsAll(c);
     }
 
     @Override
@@ -63,13 +85,8 @@ public class OverlayList<E> implements List<E>, ProxyCollection<E> {
     }
 
     @Override
-    public boolean remove(Object o) {
+    public void add(int index, E element) {
         throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean containsAll(Collection<?> c) {
-        return proxy.containsAll(c);
     }
 
     @Override
@@ -83,8 +100,30 @@ public class OverlayList<E> implements List<E>, ProxyCollection<E> {
     }
 
     @Override
+    public boolean remove(Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public E remove(int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean removeIf(Predicate<? super E> filter) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
     public boolean removeAll(Collection<?> c) {
         throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void replaceAll(UnaryOperator<E> operator) {
+        for (int i = 0, size = size(); i < size; i++) {
+            set(i, operator.apply(front.get(i)));
+        }
     }
 
     @Override
@@ -98,58 +137,58 @@ public class OverlayList<E> implements List<E>, ProxyCollection<E> {
     }
 
     @Override
-    public E get(int index) {
-        return proxy.get(index);
-    }
-
-    @Override
-    public E set(int index, E element) {
-        if (element == null) {
-            if (front.get(index) != null) {
-                front.set(index, null);
-                return proxy.set(index, back.get(index));
-            }
-            return front.get(index);
-        }
-        else {
-            front.set(index, element);
-            return proxy.set(index, element);
-        }
-    }
-
-    @Override
-    public void add(int index, E element) {
+    public void sort(Comparator<? super E> c) {
         throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public E remove(int index) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public int indexOf(Object o) {
-        return proxy.indexOf(o);
-    }
-
-    @Override
-    public int lastIndexOf(Object o) {
-        return proxy.lastIndexOf(o);
-    }
-
-    @Override
-    public ListIterator<E> listIterator() {
-        return proxy.listIterator();
-    }
-
-    @Override
-    public ListIterator<E> listIterator(int index) {
-        return proxy.listIterator(index);
     }
 
     @Override
     public List<E> subList(int fromIndex, int toIndex) {
-        return proxy.subList(fromIndex, toIndex);
+        return front.subList(fromIndex, toIndex);
+    }
+
+    @Override
+    public Object[] toArray() {
+        return front.toArray();
+    }
+
+    public <T> T[] toArray(T[] a) {
+        return front.toArray(a);
+    }
+
+    @Override
+    public <T> T[] toArray(IntFunction<T[]> generator) {
+        //noinspection SuspiciousToArrayCall
+        return front.toArray(generator);
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return front.iterator();
+    }
+
+    @Override
+    public ListIterator<E> listIterator() {
+        return front.listIterator();
+    }
+
+    @Override
+    public ListIterator<E> listIterator(int index) {
+        return front.listIterator(index);
+    }
+
+    @Override
+    public Spliterator<E> spliterator() {
+        return front.spliterator();
+    }
+
+    @Override
+    public Stream<E> stream() {
+        return front.stream();
+    }
+
+    @Override
+    public Stream<E> parallelStream() {
+        return front.parallelStream();
     }
 
 }
---
+++ test-java/app/zoftwhere/mutable/data/OverlayListTest.java
@@ -0,0 +1,144 @@
+package app.zoftwhere.mutable.data;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.IntFunction;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class OverlayListTest {
+
+    @Test
+    void todoCreateRealTest() {
+        var overlay = (OverlayList<String>) null;
+        var frontList = new ArrayList<String>();
+        frontList.add(null);
+        frontList.add(null);
+        var backList = new ArrayList<String>();
+        backList.add("back");
+        backList.add(null);
+
+        overlay = new OverlayList<>(frontList, backList);
+        assertNotNull(overlay);
+        assertFalse(overlay.isEmpty());
+        assertTrue(overlay.contains(null));
+
+        assertEquals(frontList.size(), overlay.size());
+        assertFalse(overlay.isEmpty());
+
+        overlay.set(0, null);
+        overlay.set(0, null);
+        overlay.set(0, "1");
+        assertEquals("1", overlay.get(0));
+
+        overlay.set(0, "1");
+        overlay.set(0, "1");
+        overlay.set(0, null);
+        assertEquals("back", overlay.get(0));
+
+        assertEquals(backList.indexOf("back"), overlay.indexOf("back"));
+        assertEquals(backList.lastIndexOf("back"), overlay.lastIndexOf("back"));
+
+        assertEquals(backList.contains("back"), overlay.contains("back"));
+        assertTrue(overlay.containsAll(backList));
+
+        try {
+            overlay.add("next");
+            fail("overlay.list.expected.unsupported.operation.exception");
+        }
+        catch (UnsupportedOperationException ignore) {}
+
+        try {
+            overlay.add(0, "next");
+            fail("overlay.list.expected.unsupported.operation.exception");
+        }
+        catch (UnsupportedOperationException ignore) {}
+
+        try {
+            overlay.addAll(List.of("1", "2", "3"));
+            fail("overlay.list.expected.unsupported.operation.exception");
+        }
+        catch (UnsupportedOperationException ignore) {}
+
+        try {
+            overlay.addAll(0, List.of("1", "2", "3"));
+            fail("overlay.list.expected.unsupported.operation.exception");
+        }
+        catch (UnsupportedOperationException ignore) {}
+
+        try {
+            overlay.remove("next");
+            fail("overlay.list.expected.unsupported.operation.exception");
+        }
+        catch (UnsupportedOperationException ignore) {}
+
+        try {
+            overlay.remove(0);
+            fail("overlay.list.expected.unsupported.operation.exception");
+        }
+        catch (UnsupportedOperationException ignore) {}
+
+        try {
+            overlay.removeIf(s -> true);
+            fail("overlay.list.expected.unsupported.operation.exception");
+        }
+        catch (UnsupportedOperationException ignore) {}
+
+        try {
+            overlay.removeAll(List.of("1", "2", "3"));
+            fail("overlay.list.expected.unsupported.operation.exception");
+        }
+        catch (UnsupportedOperationException ignore) {}
+
+        // Replace all.
+        overlay.set(0, "1");
+        assertEquals("1", overlay.get(0));
+        overlay.replaceAll(s -> null);
+        assertEquals("back", overlay.get(0));
+
+        try {
+            overlay.retainAll(List.of("1", "2", "3"));
+            fail("overlay.list.expected.unsupported.operation.exception");
+        }
+        catch (UnsupportedOperationException ignore) {}
+
+        try {
+            overlay.clear();
+            fail("overlay.list.expected.unsupported.operation.exception");
+        }
+        catch (UnsupportedOperationException ignore) {}
+
+        try {
+            //noinspection RedundantOperationOnEmptyContainer
+            overlay.sort(Comparator.naturalOrder());
+            fail("overlay.list.expected.unsupported.operation.exception");
+        }
+        catch (UnsupportedOperationException ignore) {}
+
+        assertNotNull(overlay.subList(0, 1));
+
+        var objectArray = overlay.toArray();
+        assertNotNull(objectArray);
+
+        var stringArray = overlay.toArray(new String[1]);
+        assertNotNull(stringArray);
+
+        IntFunction<String[]> generator = value -> new String[0];
+        assertNotNull(overlay.toArray(generator));
+
+        assertNotNull(overlay.iterator());
+        assertNotNull(overlay.listIterator());
+        assertNotNull(overlay.listIterator(0));
+        assertNotNull(overlay.spliterator());
+        assertNotNull(overlay.stream());
+        assertNotNull(overlay.parallelStream());
+    }
+
+}
