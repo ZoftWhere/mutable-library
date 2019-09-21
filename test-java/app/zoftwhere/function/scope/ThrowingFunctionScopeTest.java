b5e22d1ad7c8662da44431580ed18cbfb551fded
Osmund <21172332+osmundf@users.noreply.github.com>
2019-09-21 12:35:58 +0200

    Updated throwing function test to throw throwable.

--- test-java/app/zoftwhere/function/scope/ThrowingFunctionScopeTest.java
+++ test-java/app/zoftwhere/function/scope/ThrowingFunctionScopeTest.java
@@ -12,27 +12,27 @@ class ThrowingFunctionScopeTest {
     @Test
     void testScope() {
         final ThrowingFunction0<String> tf0 //
-            = () -> { throw new NullPointerException(); };
+            = () -> { throw new Throwable(); };
         try { tf0.accept(); }
         catch (Throwable ignored) { }
 
         final ThrowingFunction1<String, String> tf1 //
-            = (t1) -> { throw new NullPointerException(); };
+            = (t1) -> { throw new Throwable(); };
         try { tf1.accept("s1"); }
         catch (Throwable ignored) { }
 
         final ThrowingFunction2<String, String, String> tf2 //
-            = (t1, t2) -> { throw new NullPointerException(); };
+            = (t1, t2) -> { throw new Throwable(); };
         try { tf2.accept("s1", "s2"); }
         catch (Throwable ignored) { }
 
         final ThrowingFunction3<String, String, String, String> tf3 //
-            = (t1, t2, t3) -> { throw new NullPointerException(); };
+            = (t1, t2, t3) -> { throw new Throwable(); };
         try { tf3.accept("s1", "s2", "s3"); }
         catch (Throwable ignored) { }
 
         final ThrowingFunction4<String, String, String, String, String> tf4 //
-            = (t1, t2, t3, t4) -> { throw new NullPointerException(); };
+            = (t1, t2, t3, t4) -> { throw new Throwable(); };
         try { tf4.accept("s1", "s2", "s3", "s4"); }
         catch (Throwable ignored) { }
     }
