0372cfb563cb0d55fc8dcfe45472fddfe7131787
Osmund <21172332+osmundf@users.noreply.github.com>
2020-10-11 22:56:16 +0200

    Updated code coverage for Place Holder interface default methods.

--- test-java/app/zoftwhere/mutable/function/PlaceHolderTest.java
+++ test-java/app/zoftwhere/mutable/function/PlaceHolderTest.java
@@ -7,36 +7,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class PlaceHolderTest {
 
     @Test
     void testIsEmpty() {
-        final var placeHolder = new PlaceHolder<String>() {
-            String internal = null;
-
-            @Override
-            public Optional<String> optional() {
-                return Optional.empty();
-            }
-
-            @Override
-            public boolean isPresent() {
-                return internal != null;
-            }
-
-            @Override
-            public Void set(String value) {
-                internal = value;
-                return null;
-            }
-
-            @Override
-            public String get() {
-                return internal;
-            }
-        };
+        final var placeHolder = new NullableString();
 
         assertNotNull(placeHolder);
         assertFalse(placeHolder.isPresent());
@@ -44,6 +22,45 @@ class PlaceHolderTest {
 
         placeHolder.set("test");
         assertEquals("test", placeHolder.get());
+    }
+
+    @Test
+    void testGetValue() {
+        final var placeHolder = new NullableString();
+
+        assertNull(placeHolder.getValue());
+    }
+
+    @Test
+    void testOrElse() {
+        final var placeHolder = new NullableString();
+
+        assertEquals("else", placeHolder.orElse("else"));
+    }
+
+    private static class NullableString implements PlaceHolder<String> {
+        String internal = null;
+
+        @Override
+        public Optional<String> optional() {
+            return Optional.empty();
+        }
+
+        @Override
+        public boolean isPresent() {
+            return internal != null;
+        }
+
+        @Override
+        public Void set(String value) {
+            internal = value;
+            return null;
+        }
+
+        @Override
+        public String get() {
+            return internal;
+        }
     }
 
 }
